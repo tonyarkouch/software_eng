4a0d64b4f91803d5a187bc0e0b6643a00c09cd79
Erick Lozano <fenroxen@gmail.com>
2023-04-27 15:39:45 -0500

    added the scv to 2d vector function

--- software_eng/software_eng.cpp
+++ software_eng/software_eng.cpp
@@ -5,7 +5,44 @@
 //pushing to test .txt upload
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+
 using namespace std;
+
+
+vector<vector<string>> fileVector(const char* csv_name)
+{
+    ifstream fileName;
+    fileName.open(csv_name);
+    string d;
+    vector <string> myVec;
+    vector<vector<string> > data;
+
+    int i = 0;
+
+    while (!fileName.eof()) {
+
+        fileName >> d;
+        stringstream ss(d);
+        vector <string> myVec;
+        string hold;
+
+        while (getline(ss, hold, ',')) {
+            myVec.push_back(hold);
+        }
+
+        data.push_back(myVec);
+        i += 1;
+    }
+    data.erase(data.end() - 1);
+
+    fileName.close();
+    return data;
+
+}
 
 class Patient
 {

821fa08522c7f6062bda425097c4581409b56ed0
tony arkouch <tony.arkouch@gmail.com>
2023-04-27 08:09:01 -0500

    update with generic user io

--- software_eng/software_eng.cpp
+++ software_eng/software_eng.cpp
@@ -1,10 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class user_io {
+public:
+    
+    // Menu function
+    void menu() {
+        int choice;
+
+        do {
+            cout << "==== Menu ====" << endl;
+            cout << "if you are a patient please enter 1" << endl;
+            cout << "if you are a staff member please enter 2" << endl;
+            cout << "if you are a doctor please enter 3" << endl;
+            cout << "to exit menue please enter 4" << endl;
+            cout << "Enter your choice: ";
+            cin >> choice;
+
+            switch (choice) {
+            case 1:
+                //reserved to call patient menu function
+                break;
+            case 2:
+               //reserved to call staff member menue function
+                break;
+            case 3:
+                //reserved to call the doctor menue function
+                break;
+            case 4:
+                cout << "Thank you for visiting!" << endl;
+                break;
+            default:
+                cout << "Invalid choice! Try again." << endl;
+            }
+        } while (choice != 4);
+    }
+};
+
+int main() {
+    user_io io;
+    io.menu();
+    return 0;
 }
+
+
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu

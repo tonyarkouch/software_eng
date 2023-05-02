a3838e03ffe4db5855114ab57cd4e041a5ec8630
FatimaTanvir <108242439+FatimaTanvir@users.noreply.github.com>
2023-05-01 20:42:18 -0500

    Comments added
    THIS IS FUN!

--- software_eng/Notifications.h
+++ software_eng/Notifications.h
@@ -1,15 +1,16 @@
+// Header guard to prevent multiple includes of the same file
 #ifndef NOTIFICATIONS
 #define NOTIFICATIONS
-#include <string> 
+#include <string> // include necessary header files
 
 using namespace std;
 
 class Notifications
 {
 private:
-    string notificationText;
+    string notificationText; // private member variable
 public:
-    
+    // Public member functions for viewing and sending notifications to doctors and patients
     void viewNotificationsDoctors();
     void sendNotificationsDoctors();
     void viewNotificationsPatients();
